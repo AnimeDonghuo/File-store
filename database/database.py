9f75da400a2ac9380cf9ab243c96991bc841007d
MovieStar005 <152777826+MovieStar005@users.noreply.github.com>
2025-02-09 16:44:49 +0530

    Add files via upload

--- database/database.py
+++ database/database.py
@@ -1,10 +1,11 @@
 import motor.motor_asyncio
-from config import DB_URI, DB_NAME
+from config import DB_URI, DB_NAME, ADMINS
 
 dbclient = motor.motor_asyncio.AsyncIOMotorClient(DB_URI)
 database = dbclient[DB_NAME]
 
 user_data = database['users']
+req_one = database['requested_users']  # Define the collection for requested users
 
 default_verify = {
     'is_verified': False,
@@ -16,12 +17,7 @@ default_verify = {
 def new_user(id):
     return {
         '_id': id,
-        'verify_status': {
-            'is_verified': False,
-            'verified_time': "",
-            'verify_token': "",
-            'link': ""
-        }
+        'verify_status': default_verify
     }
 
 async def present_user(user_id: int):
@@ -31,7 +27,6 @@ async def present_user(user_id: int):
 async def add_user(user_id: int):
     user = new_user(user_id)
     await user_data.insert_one(user)
-    return
 
 async def db_verify_status(user_id):
     user = await user_data.find_one({'_id': user_id})
@@ -49,4 +44,23 @@ async def full_userbase():
 
 async def del_user(user_id: int):
     await user_data.delete_one({'_id': user_id})
-    return
+
+async def is_requested_one(message):
+    user = await get_req_one(message.from_user.id)
+    if user or message.from_user.id in ADMINS:
+        return True
+    return False
+
+async def add_req_one(user_id):
+    try:
+        if not await get_req_one(user_id):
+            await req_one.insert_one({"user_id": int(user_id)})
+    except:
+        pass
+        
+async def get_req_one(user_id):
+    return await req_one.find_one({"user_id": int(user_id)})
+
+async def delete_all_one():
+    await req_one.delete_many({})
+    
